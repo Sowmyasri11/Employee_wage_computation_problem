eb3b62c4827d3ad0352cb8761d63c569e0254f25
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-02-18 11:20:30 +0530

    UC1: Implemented employee attendance check

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main
@@ -5,5 +7,19 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Welcome to Employee Wage Computation ! ");
+        checkAttendance();
+    }
+
+    public static void checkAttendance(){
+        Random rand = new Random();
+        int randNUm=rand.nextInt(2);
+        int IS_PRESENT = randNUm;
+        int empcheck = (int)(Math.random() * 2);
+        if(empcheck==IS_PRESENT){
+            System.out.println("Employee is present !");
+        }
+        else{
+            System.out.println("Employee is not present !");
+        }
     }
 }
